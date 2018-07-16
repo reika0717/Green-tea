34cfac31b2a5234590f39579086e8974add4704f
reika0717 <reikahirahara@gmail.com>
2018-07-16 14:06:19 +0900

    add strikethrough list which is clicked

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,11 +1,32 @@
-import React, { Component } from 'react';
-import Form from './components/Form'
+import React, { Component } from "react";
+import Form from "./components/Form";
+import v4 from "uuid/v4";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: v4(),
+      description: "task1",
+      completed: false
+    };
+  }
+
   render() {
+    const { description, completed } = this.state;
     return (
       <div className="App">
         <Form />
+        <ul>
+          <li
+            onClick={() => {
+              this.setState({ completed: !completed });
+            }}
+            style={{ textDecoration: completed ? "line-through" : "none" }}
+          >
+            {description}
+          </li>
+        </ul>
       </div>
     );
   }
