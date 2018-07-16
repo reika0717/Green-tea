6742926047da9c99c273db4ff5a38b6107422029
reika0717 <reikahirahara@gmail.com>
2018-07-16 13:18:05 +0900

    add form and botton

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Form from './components/Form'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World</h1>
+        <Form />
       </div>
     );
   }
